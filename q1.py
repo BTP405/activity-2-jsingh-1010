14abfaa0892784792e78a0245dd155d44346628b
jsingh-1010 <158524137+jsingh-1010@users.noreply.github.com>
2024-02-14 19:53:46 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,4 +1,10 @@
 def getPrimeNumbers(num):
+    """ 
+    Determine whether a given number is a prime number.
+    The argument is the number (int) that has to be checked.
+    Returns: bool: True in the event that the number is prime; false otherwise.
+    """
+    
     if num > 1:
         for i in range(2, num):
             if num % i == 0:
@@ -8,7 +14,7 @@ def getPrimeNumbers(num):
     else:
         return False
 
-
+# Input from user
 n = int(input("Enter the desired number: "))
 if getPrimeNumbers(n):
     print("The number is a prime number")
